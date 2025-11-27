2e5f3cefde5f5a924fd0991395bbd81c2f75392a
dbswjd2825 <157003346+dbswjd2825@users.noreply.github.com>
2025-11-28 02:28:29 +0900

    publicPath: /portfolio-2025/ 추가

--- quasar.config.js
+++ quasar.config.js
@@ -43,7 +43,7 @@ module.exports = configure(function (/* ctx */) {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node20",
       },
-
+      publicPath: "/portfolio-2025/",
       vueRouterMode: "history", // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
