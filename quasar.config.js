c13a95fdee88dce7a5580a99762a2939a2e5cefc
dbswjd2825 <157003346+dbswjd2825@users.noreply.github.com>
2025-11-28 09:57:47 +0900

    publocpath 설정

--- quasar.config.js
+++ quasar.config.js
@@ -43,8 +43,9 @@ module.exports = configure(function (/* ctx */) {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node20",
       },
+      vueRouterMode: "history",
+      // available values: 'hash', 'history'
       publicPath: "/portfolio-2025/",
-      vueRouterMode: "history", // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
